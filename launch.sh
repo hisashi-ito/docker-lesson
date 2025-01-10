6562b2531e4d094ce0b3a07db68eab9e72495221
Hisashi Itoh <it.hisas@gmail.com>
2025-01-10 09:06:50 +0000

    typo

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 IMAGE="docker-lesson"
 CONTAINER="docker-lesson"
-sudo docker run -tid --gpus all --privileged --name ${CONTAINER} ${IMAGE} /sbin/init
+sudo docker run -tid --privileged --name ${CONTAINER} ${IMAGE} /sbin/init
